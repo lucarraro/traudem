e286cb82e6effa57ca091d39ebda4a9679a0237a
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-09-05 09:03:14 +0200

    feat: sitrep function

---
+++ R/sitrep.R
@@ -0,0 +1,43 @@
+#' TauDEM situation report
+#'
+#' Checks installation of TauDEM and provides useful hints.
+#'
+#' @return None
+#' @export
+#'
+#' @section TauDEM installation and registration:
+#'
+#' Once you have installed TauDEM add an environment variable
+#' pointing to the correct path.
+#' For instance on Ubuntu it could be adding this line in `.Renviron`
+#' (see `usethis::edit_r_environ()`)
+#' and then re-starting R:
+#'
+#' ```
+#' TAUDEM_PATH='/usr/local/taudem'
+#' ```
+#'
+#' or, for just the session, running this line of R code:
+#'
+#' ```r
+#' Sys.setenv(TAUDEM_PATH='/usr/local/taudem')
+#' ```
+#'
+#' @examples
+#' \dontrun{
+#' taudem_sitrep()
+#' }
+taudem_sitrep <- function() {
+  if (!nzchar(Sys.getenv("TAUDEM_PATH"))) {
+    rlang::abort(
+      message = c(
+        x = "Can't find `TAUDEM_PATH` environment variable",
+        i = "Add `TAUDEM_PATH` environment variable pointing to TauDEM executables. See `?taudem_sitrep`"
+      )
+    )
+  }
+
+  # TODO list algorithms
+
+  # TODO run hello world
+}
