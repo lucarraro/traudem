d2f9e4de13c2feecabca30b71ccc048f3adf7350
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-09-19 17:00:01 +0200

    try

--- tests/testthat/helper-os.R
+++ tests/testthat/helper-os.R
@@ -7,7 +7,7 @@ setup_os <- function() {
       PATH = paste0(
         "C:\\Program Files\\Microsoft MPI\\bin;",
         "C:\\Program Files\\GDAL;",
-        "C:\\taudem;",
+        "C:\\taudem\\TauDEM537exe;",
         Sys.getenv("PATH")
       )
     )
