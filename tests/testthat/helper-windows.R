44115cce68c04627b757acc925a9d3d4601ff649
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-09-19 14:22:07 +0200

    try

---
+++ tests/testthat/helper-windows.R
@@ -0,0 +1,16 @@
+# Windows CI
+setup_windows <- function() {
+  is_ci <- nzchar(Sys.setenv("CI"))
+  is_windows <- (tolower(Sys.info()[["sysname"]]) == "windows")
+  if (is_ci && is_windows) {
+    Sys.setenv(
+      PATH = paste0(
+        "C:\Program Files\Microsoft MPI\Bin:",
+        "C:\Program Files\GDAL:",
+        "C:\taudem:",
+        Sys.getenv("PATH")
+      )
+    )
+  }
+}
+setup_windows()
