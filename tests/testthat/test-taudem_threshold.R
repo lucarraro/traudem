ca86fd7a0235cba8641dd04bc156df092a70bbb6
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-09-30 12:57:02 +0200

    one more test

--- tests/testthat/test-taudem_threshold.R
+++ tests/testthat/test-taudem_threshold.R
@@ -1,15 +1,37 @@
 test_that("taudem_aread8() works", {
+    withr::local_options(traudem.quiet = TRUE)
   test_dir <- withr::local_tempdir()
   file.copy(
-    system.file("test-data", "MED_01_01.tif", package = "traudem"),
-    file.path(test_dir, "MED_01_01.tif")
+    system.file("test-data", "DEM.tif", package = "traudem"),
+    file.path(test_dir, "DEM.tif")
   )
-  expect_snapshot({
-    output <- taudem_pitremove(file.path(test_dir, "MED_01_01.tif"))
-    outputs <- taudem_d8flowdir(output)
-    contributing_area_grid <- taudem_aread8(outputs$output_d8flowdir_grid)
-    thresholded <- taudem_threshold(contributing_area_grid)
-  },
-    transform = taudem_transform)
-  expect_true(file.exists(thresholded))
+  withr::local_dir(test_dir)
+  taudem_pitremove(
+    input_elevation_grid = "DEM.tif",
+    output_elevation_grid = "DEMfel.tif"
+  )
+
+  # D8 flow direction ####
+  taudem_d8flowdir(
+    input_elevation_grid = "DEMfel.tif",
+    output_d8flowdir_grid = "DEMp.tif",
+    output_d8slopes_grid = "DEMsd8.tif"
+  )
+
+  # D8 contributing area ####
+  taudem_aread8(
+    input_d8flowdir_grid = "DEMp.tif",
+    output_contributing_area_grid = "DEMad8.tif"
+  )
+  tA <- 1000
+  taudem_threshold(
+    input_area_grid = "DEMad8.tif",
+    output_stream_raster_grid = "DEMsrc.tif",
+    threshold_parameter = sprintf("%.2f", tA)
+  )
+
+  src <- terra::rast("DEMsrc.tif")
+
+  expect_equal(sum(terra::values(src) == 1, na.rm = T), 160)
+
 })
